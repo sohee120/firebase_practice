eb6fce19979da64b306d1835c9f86a8264962b56
sohee120 <yuhsohee120@gmail.com>
2022-01-08 21:51:15 +0900

    feat: Login

--- 18Firebase/18Firebase/ViewController.swift
+++ 18Firebase/18Firebase/ViewController.swift
@@ -22,12 +22,41 @@ class ViewController: UIViewController {
     func updateUI() {
         if let user = Auth.auth().currentUser {
             //로그인 된 상태 (세션지원 - 자동로그인 지원)
-            labelLogin.text = "로그인상태: \(user.email!)"
+            labelLogin.text = "로그인상태: \(user.email!)로 로그인됨."
         } else {
             //로그아웃 된 상태
             labelLogin.text = "로그인상태: 로그아웃"
         }
     }
+    
+    @IBAction func onBtnLogin(_ sender: UIButton) {
+        if let textID = textfieldID.text, let textPW = textfieldPW.text {
+            
+            if textID.count < 1 || textPW.count < 1 {
+            
+                print("아이디나 암호가 너무 짧습니다.")
+                return
+            }
+            Auth.auth().signIn(withEmail: textID, password: textPW) {
+                //후행 클로저
+                [weak self] user, error in
+                guard let _ = self else { return }
+                print("로그인 되었습니다.")
+                
+                let user = Auth.auth().currentUser
+                print( "\(user?.email), \(user?.uid)")
+                self?.updateUI()
+            }
+            
+        } else {
+            print ("로그인 계정 또는 암호를 확인해주세요")
+        }
+    }
+    
+    
+    
+    
+    
 
 }
 
